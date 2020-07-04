369bb856647ed2db47b9107e985bb552cc906d17
reviewtechinterview <review.techinterview@gmail.com>
2020-07-05 01:45:11 +0900

    add init files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack.js'
+  },
+  module: {
+    rules: [ 
+      { 
+        test: /\.m?js$/, 
+        exclude: /(node_modules|bower_components)/, 
+        use: { 
+          loader: 'babel-loader', 
+          options: { 
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          } 
+        } 
+      } 
+    ] 
+  },
+  // https://webpack.js.org/concepts/mode/#mode-development
+  mode: 'development'
+}
